6bdefa83a9868f4cb73b2b0c180020c3c5684c2a
Acer <oduvan4ik_888@mail.ru>
2024-01-30 14:57:32 +0300

    Домашнее задание по теме "Декораторы"

--- decarators.py
+++ decarators.py
@@ -22,3 +22,21 @@ def sum_three(a, b, c):
 
 result = sum_three(2, 3, 6)
 print(result)
+
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+        count = 0
+        for i in range(1, result+1):
+            if result % i == 0:
+                count += 1
+        return f"Простое {result}" if count == 2 else f"Составное {result}"
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+result = sum_three(2, 3, 6)
+print(result)
