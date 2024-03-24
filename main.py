98e74067fd93325d28d142a241a56774015672ce
Acer <oduvan4ik_888@mail.ru>
2024-03-24 18:07:28 +0300

    Домашнее задание по теме "JSON, XML,YAML"

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-file_brodsky = 'brodsky.txt'
-file = open(file_brodsky, mode='r')
-file_content = file.read()
-file.close()
-print(file_content)
+# file_brodsky = 'brodsky.txt'
+# file = open(file_brodsky, 'r')
+# # file_content = file.read()
+# # print(file_content)
+# file.close()
+# import os
+#
+# print('*' * 30)
+# bro = 'brodskySkyros.txt'
+# if not os.path.exists('myPoem.txt'):
+#     with open(file='myPoem.txt', mode='w', encoding='utf-8') as file:
+#         with open(bro, 'r', encoding='utf-8') as my_file:
+#             rea = my_file.readline()
+#             file.write(rea)
+#
+#
+# with open('myPoem.txt', 'r', encoding='utf-8') as my_file:
+#     rea = my_file.readline()
+#     print(rea)
 
-print('*' * 30)
+class InOutBlock:
 
-with open('brodskySkyros.txt', 'r') as f:
-    print(f.read())
-f.close()
+    def __enter__(self):   # Выполняется при входе
+        print('Входим в блок кода')  #
+
+    def __exit__(self, exc_type, exc_val, exc_tb):   # Выполняется при выходе
+        print(f'Выходим из блока кода {exc_type}, {exc_val}, {exc_tb}')  # В тексте упомин.ошиб.параметр кот.наход в zip
+        return True    # Возвращаем истину, чтобы погасить полёт исключения
+
+
+with InOutBlock() as in_out:  # Программа входит в блок-кода
+    print('Работаем...')    # Выполняет первую строку
+    a = bla_bla / number    # Затем возникает ошибочная ситуация связанная с неопределённой переменной
+    print('Вычислили значение')   # Ошибка обрабатывается в блоке zip связанных с ошибкой
+print('После контекстного менеджера')   # особенность менеджера-возвращать значения и функции zip,
+# Если значение True - исключение обработано, его больше не надо вызывать
+
+print(InOutBlock)
+print(in_out)
+print(exit())
