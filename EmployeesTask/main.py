6eee7f2e059fc5353c4243d0e4c0950a7be0ecee
Acer <oduvan4ik_888@mail.ru>
2024-03-24 18:06:18 +0300

    Домашнее задание по теме "JSON, XML,YAML"

---
+++ EmployeesTask/main.py
@@ -0,0 +1,19 @@
+import json
+
+
+def employees_rewrite(sort_type):
+    valid_keys = {'firstName', 'lastName', 'department', 'salary'}
+    if sort_type not in valid_keys:
+        raise ValueError('Bad key for sorting')
+
+    with open('employees.json', 'r') as file:
+        data = json.load(file)
+
+    sorted_data = sorted(data['employees'], key=lambda x: x[sort_type])
+
+    with open(f'employees_{sort_type}_sorted.json', 'w') as file:
+        json.dump({'employees': sorted_data}, file, indent=2)
+
+
+
+employees_rewrite('lastName')
